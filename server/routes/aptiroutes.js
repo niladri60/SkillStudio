99a2c5e833849e83d96e4d75ed6650b676beb917
pro12365 <priyanshurouth@gmail.com>
2024-03-27 13:44:33 +0530

    upload question added and users can get questions from it

--- server/routes/aptiroutes.js
+++ server/routes/aptiroutes.js
@@ -1,8 +1,9 @@
 const express= require('express')
-const {getAptitudeQuestion, insertUploadedQuestion}= require('../controllers/apticontroller')
+const {getAptitudeQuestion, insertUploadedQuestion,getuploadedQuestion}= require('../controllers/apticontroller')
 //Initializing app router
 const router= express.Router()
 router.get('/aptitude',getAptitudeQuestion)
 router.post('/upload',insertUploadedQuestion)
+router.get('/apti_questions',getuploadedQuestion)
 //Export Router
 module.exports= router
