d21deda68d8efe25dfcd421924bd06bc85893d14
niladri <niladri.stud@gmail.com>
2024-04-17 20:42:32 +0530

    Add new file

--- server/routes/aptiroutes.js
+++
@@ -1,9 +0,0 @@
-const express= require('express')
-const {getAptitudeQuestion, insertUploadedQuestion,getuploadedQuestion}= require('../controllers/apticontroller')
-//Initializing app router
-const router= express.Router()
-router.get('/aptitude',getAptitudeQuestion)
-router.post('/upload',insertUploadedQuestion)
-router.get('/apti_questions',getuploadedQuestion)
-//Export Router
-module.exports= router
